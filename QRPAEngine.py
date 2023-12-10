4829fc94f9ba98cc1a345de095194d81eee711fd
Dogan Golcuk <dogangolcuk@gmail.com>
2023-12-10 14:10:09 +0300

    listen on app start

--- QRPAEngine.py
+++ QRPAEngine.py
@@ -1,24 +1,26 @@
 import sys
 import os
 import pystray
-from PIL import Image
-import subprocess
 import threading
+from PIL import Image
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-libs_path = os.path.join(current_dir, 'libs')
-utils_path = os.path.join(libs_path, 'utils')
 
-sys.path.append(libs_path)
-sys.path.append(utils_path)
 
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+LIBS_PATH = os.path.join(CURRENT_DIR, 'libs')
+UTILS_PATH = os.path.join(LIBS_PATH, 'utils')
+
+sys.path.append(LIBS_PATH)
+sys.path.append(UTILS_PATH)
+
+# Custom imports
 from libs.socket_operations import start_listener_flag, stop_listener_flag
 
 socket_thread = None
 
 def start_socket(icon, item):
     global socket_thread
-    if socket_thread is None or not socket_thread.is_alive():
+    if not socket_thread or not socket_thread.is_alive():
         socket_thread = threading.Thread(target=start_listener_flag)
         socket_thread.daemon = True
         socket_thread.start()
@@ -27,56 +29,50 @@ def start_socket(icon, item):
         print("Socket already running.")
 
 def stop_socket(icon, item):
-    print("Stopping socket...")
     global socket_thread
     if socket_thread and socket_thread.is_alive():
-        print("Stopping socket...2")
-        # socket_thread.join()
-        stop_listener_flag(None)  # Call the function to stop the socket listener
-        print("Stopping socket...3")
-        # socket_thread.join()  # Wait for the thread to complete
-        print("Stopping socket...4")
+        stop_listener_flag(None)
         socket_thread = None
-        print("Socket stopped.")
         icon.menu = update_menu()
+        print("Socket stopped.")
     else:
         print("Socket is not running.")
 
 def on_exit(icon, item):
-    print("Exiting...")
-    # subprocess.run(["python", "sendflow.py", os.path.join("tasks", "stopAgent.json")])
-    stop_listener_flag(None)  # Call the function to stop the socket listener
-    
-    icon.stop()  # Stop the system tray icon
+    stop_listener_flag(None)
+    icon.stop()
     sys.exit(0)
 
 def on_about(icon, item):
-    print("About this app...")
+    print("POC of QRPA Engine app...")
 
 def update_menu():
     global socket_thread
     menu_items = [
         pystray.MenuItem("Start Socket", start_socket),
         pystray.MenuItem("Stop Socket", stop_socket),
-        pystray.MenuItem("Exit", on_exit),
-        pystray.MenuItem("About", on_about)
+        pystray.MenuItem("About", on_about),
+        pystray.MenuItem("Exit", on_exit)
+        
     ]
 
-    if socket_thread and socket_thread.is_alive():
-        menu_items[0] = pystray.MenuItem("Start Socket (Running)", start_socket)
-    else:
-        menu_items[1] = pystray.MenuItem("Stop Socket (Not Running)", stop_socket)
+    start_socket_text = "Start Socket (Running)" if socket_thread and socket_thread.is_alive() else "Start Socket"
+    stop_socket_text = "Stop Socket (Not Running)" if not start_socket_text == "Start Socket (Running)" else "Stop Socket"
+    
+    menu_items[0] = pystray.MenuItem(start_socket_text, start_socket)
+    menu_items[1] = pystray.MenuItem(stop_socket_text, stop_socket)
 
     return tuple(menu_items)
 
 def main(icon):
+    start_socket(icon, None)  # Start the socket listener
     icon.menu = update_menu()
     icon.visible = True
-    # start_socket(icon,icon.menu)
+
 if __name__ == "__main__":
-    icon_path = "icons8-robot-94.png"  # Replace with your icon path
-    icon = pystray.Icon("MyApp", Image.open(icon_path), "MyApp")
+    ICON_PATH = "icons8-robot-94.png"
+    icon = pystray.Icon("QRPA Engine", Image.open(ICON_PATH), "QRPA Engine")
     icon.menu = update_menu()
-    icon.title = "MyApp"
+    icon.title = "QRPA Engine"
     
     icon.run(main)
