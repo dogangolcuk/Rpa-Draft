2d46a37fb9f3b3e75609aba5be79ec7f5e558277
Dogan Golcuk <dogangolcuk@gmail.com>
2023-12-10 13:30:11 +0300

    system tray added to start and stop socket listening

--- QRPAEngine.py
+++ QRPAEngine.py
@@ -12,33 +12,71 @@ utils_path = os.path.join(libs_path, 'utils')
 sys.path.append(libs_path)
 sys.path.append(utils_path)
 
-from libs.socket_operations import start_socket_listener
+from libs.socket_operations import start_listener_flag, stop_listener_flag
 
-def main():
-    # Start socket listener in a separate thread
-    socket_thread = threading.Thread(target=start_socket_listener)
-    socket_thread.daemon = True  # Daemonize the thread
-    socket_thread.start()
+socket_thread = None
+
+def start_socket(icon, item):
+    global socket_thread
+    if socket_thread is None or not socket_thread.is_alive():
+        socket_thread = threading.Thread(target=start_listener_flag())
+        socket_thread.daemon = True
+        socket_thread.start()
+        icon.menu = update_menu()
+    else:
+        print("Socket already running.")
+
+def stop_socket(icon, item):
+    print("Stopping socket...")
+    global socket_thread
+    if socket_thread and socket_thread.is_alive():
+        print("Stopping socket...2")
+        # socket_thread.join()
+        stop_listener_flag(None)  # Call the function to stop the socket listener
+        print("Stopping socket...3")
+        # socket_thread.join()  # Wait for the thread to complete
+        print("Stopping socket...4")
+        socket_thread = None
+        print("Socket stopped.")
+        icon.menu = update_menu()
+    else:
+        print("Socket is not running.")
 
 def on_exit(icon, item):
     print("Exiting...")
-    # Execute the script on exit
-    subprocess.run(["python", "sendflow.py", os.path.join("tasks", "stopAgent.json")])
-    # Any cleanup code you want before exiting
+    # subprocess.run(["python", "sendflow.py", os.path.join("tasks", "stopAgent.json")])
+    stop_listener_flag(None)  # Call the function to stop the socket listener
+    
     icon.stop()  # Stop the system tray icon
     sys.exit(0)
 
 def on_about(icon, item):
     print("About this app...")
-    # Show information about the application
 
-# Create the tray icon and menu
-icon_path = "icons8-automation-32.png"  # Replace with your icon path
-icon = pystray.Icon("MyApp", Image.open(icon_path), "MyApp")
-icon.menu = (pystray.MenuItem("Exit", on_exit), pystray.MenuItem("About", on_about))
+def update_menu():
+    global socket_thread
+    menu_items = [
+        pystray.MenuItem("Start Socket", start_socket),
+        pystray.MenuItem("Stop Socket", stop_socket),
+        pystray.MenuItem("Exit", on_exit),
+        pystray.MenuItem("About", on_about)
+    ]
 
-# Start the application
-main()
+    if socket_thread and socket_thread.is_alive():
+        menu_items[0] = pystray.MenuItem("Start Socket (Running)", start_socket)
+    else:
+        menu_items[1] = pystray.MenuItem("Stop Socket (Not Running)", stop_socket)
 
-# Start the system tray loop
-icon.run()
+    return tuple(menu_items)
+
+def main(icon):
+    icon.menu = update_menu()
+    icon.visible = True
+    # start_socket(icon,icon.menu)
+if __name__ == "__main__":
+    icon_path = "icons8-robot-48.png"  # Replace with your icon path
+    icon = pystray.Icon("MyApp", Image.open(icon_path), "MyApp")
+    icon.menu = update_menu()
+    icon.title = "MyApp"
+    
+    icon.run(main)
--- libs/socket_operations.py
+++ libs/socket_operations.py
@@ -7,12 +7,25 @@ from libs.image_operations import process_image_commands
 from libs.logging_utils import log_message
 
 stop_listener = False
+listening_message_displayed = False
+# should_stop = False  # Add this flag
+
+
+def start_listener_flag():
+    global stop_listener
+    # global should_stop
+    stop_listener = False
+    # should_stop = False
+    start_socket_listener()
 
 
 def stop_listener_flag(cmd):
+    # global should_stop
+    # should_stop = True
     global stop_listener
+    global listening_message_displayed
+    listening_message_displayed = False
     stop_listener = True
-    return stop_listener
 
 
 COMMAND_DISPATCHER = {
@@ -20,38 +33,65 @@ COMMAND_DISPATCHER = {
     "findImage": process_image_commands,
     "startScreenRecord": handle_screen_record,
     "stopScreenRecord": stop_screen_record,
-    "stopServerAgent": stop_listener_flag,
+    "stopServerListener": stop_listener_flag,
+    "startServerListener": start_listener_flag,
     "excelToPandas": excel_to_pandas,
     "manipulateData": manipulate_and_write_data,
 }
 
 
 def start_socket_listener():
+    global stop_listener, listening_message_displayed
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(
+        socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Set SO_REUSEADDR
+
     server_socket.bind(('127.0.0.1', 12345))
     server_socket.listen(5)
+    server_socket.settimeout(2)  # Set a timeout on accept
+    listening_message_displayed = False  # Flag to track the message display
+
 
     while not stop_listener:
-        print("Listening for commands...")
-        client_socket, address = server_socket.accept()
-        print(f"Connection from {address} established.")
-        log_message(f"Connection from {address} established.")
+        # if should_stop:
+        #     breakc
+        
 
         try:
-            commands = json.loads(client_socket.recv(1024).decode())
-            for cmd in commands:
-                log_message(f"Received command: {cmd['command']}")
-                command_function = COMMAND_DISPATCHER.get(cmd["command"])
-                if command_function:
-                    command_function(cmd)
-                    log_message(f"Executed command: {cmd['command']}")
-                else:
-                    log_message(f"Command '{cmd['command']}' not supported.")
-        except json.JSONDecodeError as e:
-            log_message(f"Error decoding JSON: {e}")
-        except Exception as ex:
-            log_message(f"Error processing commands: {ex}")
+            # print("Listening for commands...")
+            if not listening_message_displayed:
+                log_message("Listening for commands...")
+                listening_message_displayed = True
+                
+            client_socket, address = server_socket.accept()
+            print(f"Connection from {address} established.")
+            log_message(f"Connection from {address} established.")
 
-        client_socket.close()
+            try:
+                commands = json.loads(client_socket.recv(1024).decode())
+                for cmd in commands:
+                    log_message(f"Received command: {cmd['command']}")
+                    command_function = COMMAND_DISPATCHER.get(cmd["command"])
+                    if command_function:
+                        command_function(cmd)
+                        log_message(f"Executed command: {cmd['command']}")
+                        listening_message_displayed = False
+                    else:
+                        log_message(
+                            f"Command '{cmd['command']}' not supported.")
+                        listening_message_displayed = False
+            except json.JSONDecodeError as e:
+                log_message(f"Error decoding JSON: {e}")
+            except Exception as ex:
+                log_message(f"Error processing commands: {ex}")
+
+            client_socket.close()
+
+        except socket.timeout:
+            pass
+
+        if stop_listener:
+            break
 
     server_socket.close()
