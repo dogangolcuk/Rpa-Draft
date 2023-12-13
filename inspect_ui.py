a416fb9839bad519b4b49c217ea0229e9e34503f
Dogan Golcuk <dogangolcuk@gmail.com>
2023-12-13 10:10:23 +0300

    inspect.ui

--- inspect_ui.py
+++ inspect_ui.py
@@ -34,8 +34,28 @@ def print_element_info(node, index):
     print(f"ID: {id(child)}")
     print("---------------------------------------------------------")
 
+def traverse_children_with_name_and_type(element_info, target_name, target_control_type):
+    matching_children = []
+
+    for child in element_info.children():
+        if child.name == target_name and child.control_type == target_control_type:
+            matching_children.append(child)
+        
+        matching_children.extend(traverse_children_with_name_and_type(child, target_name, target_control_type))
+
+    return matching_children
+
 element_info_instance = element_info("uia")
 elements = get_next(element_info_instance)
 
 for index, node in enumerate(elements, start=1):
     print_element_info(node, index)
+
+target_name = "Program Manager"
+target_control_type = "Pane"
+matching_elements = traverse_children_with_name_and_type(element_info_instance, target_name, target_control_type)
+
+for index, element in enumerate(matching_elements, start=1):
+    print(f"Match {index}:")
+    print(f"Name: {element.name}")
+    print(f"Control type: {element.control_type}")
